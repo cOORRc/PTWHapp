940251288ce089f13cf9b7074861a6f27f0046c3
Chayapa <chayapa.pr.58@ubu.ac.th>
2023-06-02 02:06:01 +0700

    login = done scan = done เหลือทำ fragments หน้า Home

---
+++ app/src/main/java/com/oilpt/ptwhapp/LogniPage.kt
@@ -0,0 +1,11 @@
+package com.oilpt.ptwhapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogniPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
